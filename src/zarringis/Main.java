f7922e329b6533a53629a3a7580969d5bcc26241
Karina Reed <zarringis@gmail.com>
2022-03-17 13:39:41 +0300

    Освоение класса ArrayList, использование циклов

--- src/zarringis/Main.java
+++ src/zarringis/Main.java
@@ -1,19 +1,34 @@
 package zarringis;
 
 import java.io.Console;
+import java.util.ArrayList;
+
 
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Point p1 = new Point(2, 3, '*');
         p1.draw();
 
         Point p2 = new Point(4, 5, '#');
         p2.draw();
 
-    }
+        ArrayList<Integer> numList = new ArrayList<>();
+        numList.add(0);
+        numList.add(1);
+        numList.add(2);
 
+        for (int i: numList)
+        {
+            System.out.println(i);
+        }
+
+        ArrayList<Point> plist = new ArrayList<>();
+        plist.add(p1);
+        plist.add(p2);
+        System.out.println("Завершение цикла finish");
+
+    }
 
 
 }
